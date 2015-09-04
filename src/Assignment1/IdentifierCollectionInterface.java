b0f73599528c6f9cfb124abe2125d33fdc4d8ea9
Elias <elias.el.khaldi@gmail.com>
2015-09-04 16:47:16 +0200

    adv programming

---
+++ src/Assignment1/IdentifierCollectionInterface.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+/**
+	ADT for the class IdentifierCollection:
+	
+	@author Alae, Elias
+	@elements identifiers of type Identifier
+	@structure   
+		
+*/
+
+public interface IdentifierCollectionInterface {
+
+}
